504fa0000a1943d48e810b5418345a0c487166c3
Derek Villars, Melvin Gruschow and Sam Kirsch <derek+melvin+sam@>
2017-02-08 10:28:34 -0800

    flesh out the project and add basic search functionality

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,6 +1,6 @@
 ﻿using Nancy;
 using System.Collections.Generic;
-using PlacesYouBeen.Objects;
+using Places.Objects;
 
 namespace Places
 {
@@ -8,7 +8,23 @@ namespace Places
     {
         public HomeModule()
         {
+            Get["/"] = _ =>
+            {
+                return View["index.cshtml"];
+            };
 
+            Post["/places"] = _ =>
+            {
+                Place newPlace = new Place(Request.Form["location"], Request.Form["description"]);
+                return View["places.cshtml", Place.GetAll()];
+            };
+
+            Post["/search"] = _ =>
+            {
+                string searchItem = Request.Form["search"];
+                var Result = Place.GetAll().Find(item => item.GetName() == searchItem);
+                return View["search.cshtml", Result];
+            };
         }
     }
 }
--- Objects/Places.cs
+++ Objects/Places.cs
@@ -7,14 +7,21 @@ namespace Places.Objects
         private string _name;
         private string _description;
 
-        private static List<Place> PlacesList = new List<Places> { };
+        private static List<Place> PlacesList = new List<Place> { };
+
+        public Place (string name, string description)
+        {
+            SetName(name);
+            SetDescription(description);
+            PlacesList.Add(this);
+        }
 
         public string GetName()
         {
             return _name;
         }
 
-        public void SetName(newName)
+        public void SetName(string newName)
         {
             _name = newName;
         }
@@ -24,14 +31,9 @@ namespace Places.Objects
             return _description;
         }
 
-        public void SetDescription(newDescription)
+        public void SetDescription(string newDescription)
         {
             _description = newDescription;
-        }
-
-        public void AddPlace(Place newPlace)
-        {
-            PlacesList.Add(newPlace);
         }
 
         public static List<Place> GetAll()
